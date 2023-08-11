9213a2edf472913452268d31f965176d795dd856
Wilson Callisaya <demarktec@gmail.com>
2023-08-10 19:13:26 -0500

    header y banner

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+import {Header} from './components/Header/Header'
+import { Banner } from './components/Banner/Banner'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Ecommerce</h1>
+    <BrowserRouter>      
+      <Header/>
+      <Banner/>
+    </BrowserRouter>
     </>
   )
 }
